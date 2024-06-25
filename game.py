4a90addbfb9711e4dd56b31f94cec775182be9d8
scientistbodybuilder <o.jikineh@mail.utoronto.ca>
2024-06-24 20:54:42 -0400

    Added 'game over' as a new game state, having a new background and options. Added the reset of all obstacles when player quits or retriesa round, and increased the screen resolution

--- game.py
+++ game.py
@@ -1,12 +1,13 @@
 import pygame
 from sys import exit
 import random
+import time
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.transform.scale_by(pygame.image.load('Assets/Sprites/Player_Sprite/flappy.xcf').convert_alpha(), 0.5)
-        self.rect = self.image.get_rect(midbottom = (400,250))
+        self.rect = self.image.get_rect(midbottom = (640,360))
         self.gravity = 0
         # self.jump_sound = pygame.mixer.Sound('Assets/Sound/jump2.wav')
         # self.jump_sound.set_volume(0.5)
@@ -24,15 +25,15 @@ class Player(pygame.sprite.Sprite):
                 self.rect.left = 0
         if keys[pygame.K_d]:
             self.rect.x += 3
-            if self.rect.right > 800:
-                self.rect.right = 800
+            if self.rect.right > 1280:
+                self.rect.right = 1280
 
     def apply_gravity(self):
         self.gravity += 0.5
         self.rect.top += self.gravity
 
     def game_over(self):
-        if self.rect.top >= 700:
+        if self.rect.top >= 900:
             return True
 
     def update(self):
@@ -43,7 +44,7 @@ class Bean(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.transform.scale_by(pygame.image.load('Assets/Sprites/Player_Sprite/Coffee_bean.xcf').convert_alpha(), 0.4)
-        self.x_pos = random.randint(1,800)
+        self.x_pos = random.randint(1,1280)
         self.y_pos = -10
         self.rect = self.image.get_rect(midbottom = (self.x_pos,self.y_pos))
         
@@ -57,8 +58,11 @@ class Bean(pygame.sprite.Sprite):
     #      self.rect = self.image.get_rect(midbottom = (self.x_pos,self.y_pos))
 
     def destroy(self):
-        if self.rect.y > 500:
+        if self.rect.y > 720:
             self.kill()
+
+    def clear(self):
+        self.kill()
 
     def update(self):
         #self.rotate()
@@ -68,7 +72,7 @@ class Bean(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.x_pos = random.randint(1,800)
+        self.x_pos = random.randint(1,1280)
         self.y_pos = -10
         self.image = pygame.transform.scale_by(pygame.image.load('Assets/Sprites/Player_Sprite/Sugar1.xcf').convert_alpha(), 0.3)
         self.rect = self.image.get_rect(midbottom = (self.x_pos,self.y_pos))
@@ -77,8 +81,11 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.y += 2
 
     def destroy(self):
-        if self.rect.y > 500:
+        if self.rect.y > 720:
             self.kill()
+
+    def clear(self):
+        self.kill()
 
     def update(self):
         #self.rotate()
@@ -110,10 +117,10 @@ class Button():
 
         return action
 
-def display_score2():
+def display_score2(pos):
     score = collision_count
     score_surface = font.render(f'SCORE: {score}',False,WHITE)
-    score_rect = score_surface.get_rect(midtop = (100,450))
+    score_rect = score_surface.get_rect(midtop = pos) #100,675
     screen.blit(score_surface,score_rect)
 
 
@@ -133,8 +140,8 @@ def display_score2():
 
 pygame.init()
 
-width = 800
-height = 500
+width = 1280
+height = 720
 screen = pygame.display.set_mode((width, height))   #main display surface
 pygame.display.set_caption("Flappuccino")
 font = pygame.font.Font(None, 30)
@@ -151,13 +158,14 @@ flappy = Player()
 player = pygame.sprite.GroupSingle()
 player.add(flappy)
 
-game_active_background = pygame.image.load('Assets/Backgrounds/GameActiveBackground1.png')
-game_intro_background =  pygame.image.load('Assets/Backgrounds/IntroBackground1.png')
-pause_menu_packground = pygame.image.load('Assets/Backgrounds/PauseMenu1.png')
+game_active_background = pygame.image.load('Assets/Backgrounds/bkg1_1280x720.png')
+game_intro_background =  pygame.image.load('Assets/Backgrounds/titlepage2_1280x720.png')
+pause_menu_packground = pygame.image.load('Assets/Backgrounds/pause_1280x720.png')
+game_over_background = pygame.image.load('Assets/Backgrounds/gameover_bkg_1280x720.png')
 quit_button_img = pygame.image.load('Assets/Buttons/Quit Button1.png')
 resume_button_img = pygame.image.load('Assets/Buttons/Resume Button1.png')
 
-dashboard_surface = pygame.Surface((800,70))
+dashboard_surface = pygame.Surface((width,70))
 dashboard_surface.fill((102, 64, 26))
 
 bean_surface = pygame.transform.scale_by(pygame.image.load('Assets/Sprites/Player_Sprite/Coffee_bean.xcf').convert_alpha(), 0.4)
@@ -170,6 +178,9 @@ ratio = 1
 
 game_active = False
 game_paused = False
+flappy_died = False
+
+#Game Over
 rotation = 0
 collision_count = 0
 collision_sound = pygame.mixer.Sound('Assets/Sound/player-select.mp3')
@@ -178,6 +189,12 @@ game_over_sound = pygame.mixer.Sound('Assets/Sound/player-lose.wav')
 game_over_sound.set_volume(0.3)
 damage_sound = pygame.mixer.Sound('Assets/Sound/damage1-mixkit.wav')
 damage_sound.set_volume(0.5)
+
+Intro_suface = font.render(f'Press Space to Start',False,GREY)
+Intro_rect = Intro_suface.get_rect(midbottom = (300,360))
+Controls_surface = font.render("Use 'w,a,d' to move and press ESC to pause",False,GREY)
+controls_rect = Controls_surface.get_rect(midbottom = (300,390))
+
 
 object_timer = pygame.USEREVENT +1
 pygame.time.set_timer(object_timer, 900)
@@ -200,11 +217,26 @@ while True: # lets our code keep running forever epic. we break if the game ends
                 if event.type == object_timer and game_active:
                     obstacles.add(Bean())
                     obstacles.add(Enemy())
+            else:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        game_paused = False
+                    if event.key == pygame.K_q:
+                        game_paused = False
+                        game_active = False
 
         else:
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                player_gravity = 0
-                game_active = True
+            if flappy_died == True:
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    flappy_died = False
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                    collision_count=0
+                    flappy_died = False
+                    game_active = True
+            else:
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and game_active == False:
+                    player_gravity = 0
+                    game_active = True
                      
     if game_active and game_paused == False:
         screen.blit(game_active_background,(0,0))
@@ -213,18 +245,19 @@ while True: # lets our code keep running forever epic. we break if the game ends
         player.update()
         for character in player:
             if character.game_over():
+                flappy_died = True
                 game_active = False
 
         obstacles.draw(screen)
         obstacles.update()
         
-        screen.blit(dashboard_surface,(0,430))
-        screen.blit(bean_surface, (150,435))
+        screen.blit(dashboard_surface,(0,650))
+        screen.blit(bean_surface, (150,660))
 
-        pygame.draw.rect(screen, "red", (242,446,200,35))
-        pygame.draw.rect(screen, (9, 216, 39), (242,446, 200 * ratio, 35))
+        pygame.draw.rect(screen, "red", (242,667,200,35))
+        pygame.draw.rect(screen, (9, 216, 39), (242,667, 200 * ratio, 35))
         
-        display_score2()
+        display_score2((100,675))
 
         collisions = pygame.sprite.spritecollide(flappy,obstacles,True)
         for obstacle in collisions:
@@ -236,8 +269,10 @@ while True: # lets our code keep running forever epic. we break if the game ends
                 health-=1
                 ratio -= 0.2
                 if health == 0:
-                    game_active = False
                     game_over_sound.play()
+                    time.sleep(2)
+                    game_active = False
+                    flappy_died = True
     
         rotation += 3
 
@@ -245,37 +280,49 @@ while True: # lets our code keep running forever epic. we break if the game ends
         screen.blit(pause_menu_packground,(0,0))
 
         score_pause_surface = comicsans.render(f'Current Score: {collision_count}',False,BLACK)
-        score_pause_rect = score_pause_surface.get_rect(midbottom = (400,70))
+        score_pause_rect = score_pause_surface.get_rect(midbottom = (640,70))
         screen.blit(score_pause_surface,score_pause_rect)
 
-        button1 = Button(400,200, resume_button_img, 1)
+        button1 = Button(640,200, resume_button_img, 1)
         if button1.draw(): #resume button was clicked
             game_paused = False
-        button2 = Button(400,300, quit_button_img, 1)
+        button2 = Button(640,300, quit_button_img, 1)
         if button2.draw():  # quit button was clicked
             game_paused = False
             game_active = False
-        
-    else:
-        if collision_count != 0 and game_paused == False:
-            game_over_sound.play()
-        screen.blit(game_intro_background,(0,0))
-        Gameover_suface = font.render(f'Press Space to Start',False,GREY)
-        gameover_rect = Gameover_suface.get_rect(midbottom = (300,250))
 
-        Controls_surface = font.render("Use 'w,a,d' to move and press ESC to pause",False,GREY)
-        controls_rect = Controls_surface.get_rect(midbottom = (300,285))
-        screen.blit(Gameover_suface,gameover_rect)
-        screen.blit(Controls_surface,controls_rect)
-        collision_count = 0
+    elif game_active == False and flappy_died == True:
+        screen.blit(game_over_background,(0,0))
+        display_score2((300,300))
+
+        retry_surface = font.render(f'Press R to retry',False,WHITE)
+        retry_rect = retry_surface.get_rect(midbottom = (300,350))
+        home_surface = font.render(f'Press SPACE to return home',False,WHITE)
+        home_rect = home_surface.get_rect(midbottom = (300,400))
+
+        screen.blit(retry_surface,retry_rect)
+        screen.blit(home_surface,home_rect)
         health = 5
         ratio = 1
 
         #Reset the player back to the starting position
         for character in player:
-            character.rect.midbottom = (400,250)
+            character.rect.midbottom = (640,360)
             character.gravity = 0
+        obstacles.empty()
+    else:
+        screen.blit(game_intro_background,(0,0))
+        screen.blit(Intro_suface,Intro_rect)
+        screen.blit(Controls_surface,controls_rect)
+
+        health = 5
+        ratio = 1
+
+        #Reset the player back to the starting position
+        for character in player:
+            character.rect.midbottom = (640,360)
+            character.gravity = 0
+        obstacles.empty()
         
-       
     pygame.display.update()
     clock.tick(60) #won't run faster than 60 frames per second
